d1ee4193754997f6e9a42d2144ecd9a365798189
Suhyun118 <hyuni.r.118@gmail.com>
2023-06-16 00:26:13 +0900

    add canvas

---
+++ src/canvas/clnt.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <opencv2/opencv.hpp>
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+#define PORT 8080
+
+using namespace cv;
+
+void *send_canvas(void *arg);
+void *recv_canvas(void *arg);
+void error_handling(char *msg);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+Mat temp;
+Point p;
+bool isDrawing;
+int clear_flag=-2;
+
+void onMouse(int event, int x, int y, int flags, void* param)
+{
+    if (event == cv::EVENT_LBUTTONDOWN)
+    {
+        // Start drawing
+        isDrawing = true;
+        p = cv::Point(x, y);
+    }
+    else if (event == cv::EVENT_LBUTTONUP)
+    {
+        // Stop drawing
+        isDrawing = false;
+        clear_flag=-1;
+    }
+    else if (event == cv::EVENT_MOUSEMOVE && isDrawing)
+    {
+        temp = *(Mat*)param;
+
+        // Draw a line from the previous point to the current point
+        cv::Point currPt(x, y);
+        cv::line(temp, p, currPt, cv::Scalar(255, 255, 255), 3);
+        p = currPt;
+        clear_flag = 0;
+    }
+    else if (event == cv::EVENT_RBUTTONDOWN)
+    {
+        // Clear the canvas
+        temp = cv::Scalar(0, 0, 0);
+        clear_flag = 1;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in serv_addr;
+    pthread_t snd_thread, rcv_thread;
+    void *thread_return;
+    if (argc != 4)
+    {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+        exit(1);
+    }
+
+    sprintf(name, "[%s]", argv[3]);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error");
+    pthread_create(&snd_thread, NULL, send_canvas, (void*)&sock);
+    // pthread_create(&rcv_thread, NULL, recv_canvas, (void*)&sock);
+    pthread_join(snd_thread, &thread_return);
+    // pthread_join(rcv_thread, &thread_return);
+    close(sock);
+    //printf("closed\n");
+    return 0;
+}
+
+void *send_canvas(void *arg)   // send thread main
+{
+    int sock = *((int*)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+    int x, y;
+
+    namedWindow("Canvas", WINDOW_NORMAL);
+    Mat canvas = Mat::zeros(600, 800, CV_8UC3);
+    setMouseCallback("Canvas", onMouse, &canvas);
+
+    while (1)
+    {
+        // Display the canvas with the drawing
+        imshow("Canvas", canvas);
+
+
+        if(clear_flag==1) {
+            x = -20;
+            y = -20;
+            write(sock, &x, sizeof(x));
+            write(sock, &y, sizeof(y));
+        }
+        else if(clear_flag==-1) {
+            x = -10;
+            y = -10;
+            write(sock, &x, sizeof(x));
+            write(sock, &y, sizeof(y));
+        }
+        else if(clear_flag==0) {
+            // Send the x and y coordinates of the mouse
+            x = p.x;
+            y = p.y;
+            write(sock, &x, sizeof(x));
+            write(sock, &y, sizeof(y));
+        }
+        
+
+        waitKey(15);
+    }
+    return NULL;
+}
+
+void *recv_canvas(void *arg)   // read thread main
+{
+    int sock = *((int*)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+    int str_len;
+    while (1)
+    {
+        str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1);
+        if (str_len == -1)
+            return (void*)-1;
+        name_msg[str_len] = 0;
+        fputs(name_msg, stdout);
+    }
+    return NULL;
+}
+
+void error_handling(char *msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ src/canvas/serv.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include "opencv4/opencv2/opencv.hpp"
+
+using namespace cv;
+
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+void *handle_clnt(void *arg);
+void send_msg(char *msg, int len);
+void error_handling(char *msg);
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+
+Mat canvas;
+
+int main(int argc, char *argv[])
+{
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    int clnt_adr_sz;
+    pthread_t t_id;
+    
+    if (argc != 2)
+    {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    pthread_mutex_init(&mutx, NULL);
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("bind() error");
+    if (listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    while (1)
+    {
+        // Accept client connections
+        clnt_adr_sz=sizeof(clnt_adr);
+        if ((clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, (socklen_t *)&clnt_adr_sz)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_mutex_lock(&mutx);
+        clnt_socks[clnt_cnt++] = clnt_sock;
+        pthread_mutex_unlock(&mutx);
+
+        pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
+        pthread_detach(t_id);
+        printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void *handle_clnt(void *arg)
+{
+    int clnt_sock = *((int *)arg);
+
+    namedWindow("Received Drawing", WINDOW_NORMAL);
+    canvas = Mat::zeros(600, 800, CV_8UC3);
+    Point prevPoint(-1, -1);
+
+    while(1){
+        // Receive the size of the drawing data from the client
+        int x;
+        int y;
+
+        // 클라이언트로부터 x, y 좌표 읽기
+        if (read(clnt_sock, &x, sizeof(x)) == -1)
+            return (void *)-1;
+
+        if (read(clnt_sock, &y, sizeof(y)) == -1)
+            return (void *)-1;
+
+        // printf("x: %d and y: %d\n", x, y);
+
+        if(x==-20 && y==-20) {
+            canvas = Scalar(0,0,0);
+        }
+        else if(x==-10 && y==-10) {
+            prevPoint.x = -1;
+            prevPoint.y = -1;
+        }
+        else {
+            Point currPoint(x, y);
+
+            if (prevPoint.x != -1 && prevPoint.y != -1) {
+                line(canvas, prevPoint, currPoint, Scalar(255, 255, 255), 3);
+            }
+
+            prevPoint = currPoint;
+        }
+
+        // Display the received drawing
+        imshow("Received Drawing", canvas);
+        waitKey(15);
+    }
+    
+    return NULL;
+}
+
+void send_msg(char *msg, int len) // send to all
+{
+    int i;
+    pthread_mutex_lock(&mutx);
+    for (i = 0; i < clnt_cnt; i++)
+        write(clnt_socks[i], msg, len);
+    pthread_mutex_unlock(&mutx);
+}
+
+void error_handling(char *msg)
+{
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+/*void show_rank() {
+    for (i = 0; i < clnt_cnt; i++)
+    {
+        printf("===== RANK =====");
+        for (i = 0; i < clnt_cnt; i++)
+        {
+            print()
+        }
+        printf("================");
+    }
+}*/
